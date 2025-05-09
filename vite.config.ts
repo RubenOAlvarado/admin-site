230c34015bde9d43d3c944e51160904c8b9c6d8f
Ruben Alvarado <ruben.alvardo@plexusworldwide.com>
2025-05-09 11:07:27 -0600

    refactor(vite.config.ts): remove redundant configuration settings
    The configuration settings for `define`, `base`, `server`, and `build` were removed as they are no longer necessary. The project now relies solely on the default Vite configuration and plugins, simplifying the setup.

--- vite.config.ts
+++ vite.config.ts
@@ -4,22 +4,5 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  define: {
-    'import.meta.env.API_BASE_URL': JSON.stringify(
-      process.env.NODE_ENV === 'production'
-        ? 'http://caddy/api'
-        : 'http://localhost/api'
-    )
-  },
-  base: '/admin/',
-  server: {
-    port: 3000,
-  },
-  build: {
-    outDir: './dist/admin',
-    assetsDir: 'assets',
-    emptyOutDir: true,
-    sourcemap: true,
-  },
   plugins: [react(), tailwindcss()],
 })
